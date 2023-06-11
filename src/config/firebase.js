090e13c1e61a4d2cb0f6775d6862ca8e2e70dbae
Jason M <46291780+JasonMorta@users.noreply.github.com>
2023-06-11 19:17:49 +0200

    updates

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,10 +8,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"; //getAuth is the authentication object
 import { getFirestore } from "firebase/firestore"; //Get Cloud Firestore database
 
-
+console.log('process.env.REACT_APP_API_KEY', process.env.REACT_APP_API_KEY)
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
+
 
   authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
   projectId: process.env.REACT_APP_PROJECT_ID,
